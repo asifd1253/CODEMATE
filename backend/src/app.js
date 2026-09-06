fe79bedc705bcee01c8b0baea9fa313402fb9b2d
Asif <asifd@gmail.com>
2026-09-06 12:37:16 +0530

    EPISODE-05: Multiple Route Handler

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,9 +9,29 @@ app.use(
     res.send("response from 1st function");
     next();
   },
-  (req, res) => {
+  [(req, res, next) => {
     console.log("2nd response");
-    res.send("reponse form 2nd function");
+    // res.send("response from 2nd function");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3rd response");
+    // res.send("response from 3rd function");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4th response");
+    // res.send("response from 4th function");
+    next();
+  }],
+  (req, res, next) => {
+    console.log("5th response");
+    // res.send("response from 5th function");
+    next();
+  },
+  (req, res) => {
+    console.log("6th response");
+    // res.send("reponse form th function");
   },
 );
 
