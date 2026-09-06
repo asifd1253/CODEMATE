81b2f0628b9038c3bf58e22fef681af0e294388b
Asif <asifd@gmail.com>
2026-09-06 12:27:22 +0530

    EPISODE-05: Multiple Route Handler

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1st response");
+    res.send("response from 1st function");
+    next();
+  },
+  (req, res) => {
+    console.log("2nd response");
+    res.send("reponse form 2nd function");
+  },
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
